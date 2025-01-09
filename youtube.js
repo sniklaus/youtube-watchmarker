304659a485d4036dfe682386e8dcb9462b5b98b4
sniklaus <sniklaus@adobe.com>
2025-01-08 20:57:13 -0800

    no message

--- youtube.js
+++ youtube.js
@@ -2,49 +2,34 @@
 
 let strLastchange = null;
 let intWatchdate = {};
-const videoObserverMap = new WeakMap();
+let objObservers = new WeakMap();
 
 // ##########################################################
 
-function getVideos(id = "") {
-    const document = window.document;
-    const selectors = [
-        // Regular thumbnails.
-        "a.ytd-thumbnail[href^=\"/watch?v=" + id + "\"]",
-        "a.yt-lockup-view-model-wiz__content-image[href^=\"/watch?v=" + id + "\"]",
-        "ytd-compact-video-renderer a.yt-simple-endpoint[href^=\"/watch?v=" + id + "\"]",
+let videos = function(strIdent) {
+    return Array.from(window.document.querySelectorAll(([
+        'a.ytd-thumbnail[href^="/watch?v=' + strIdent + '"]', // regular
+        'a.yt-lockup-view-model-wiz__content-image[href^="/watch?v=' + strIdent + '"]', // regular
+        'ytd-compact-video-renderer a.yt-simple-endpoint[href^="/watch?v=' + strIdent + '"]', // regular
+        'a.ytp-ce-covering-overlay[href*="/watch?v=' + strIdent + '"]', // overlays
+        'a.ytp-videowall-still[href*="/watch?v=' + strIdent + '"]', // videowall
+        'a.ytd-thumbnail[href^="/shorts/' + strIdent + '"]', // shorts
+        'a.ShortsLockupViewModelHostEndpoint[href^="/shorts/' + strIdent + '"]', // shorts
+        'a.reel-item-endpoint[href^="/shorts/' + strIdent + '"]', // shorts
+        'a.media-item-thumbnail-container[href^="/watch?v=' + strIdent + '"]', // mobile
+    ]).join(', ')));
+};
 
-        // Video overlays.
-        "a.ytp-ce-covering-overlay[href*=\"/watch?v=" + id + "\"]",
+let refresh = function() {
+    let objVideos = videos('');
 
-        // Video wall.
-        "a.ytp-videowall-still[href*=\"/watch?v=" + id + "\"]",
-
-        // Shorts.
-        "a.ytd-thumbnail[href^=\"/shorts/" + id + "\"]",
-        "a.ShortsLockupViewModelHostEndpoint[href^=\"/shorts/" + id + "\"]",
-        "a.reel-item-endpoint[href^=\"/shorts/" + id + "\"]"
-    ];
-    const elements = Array.from(document.querySelectorAll(selectors.join(", ")));
-    return elements;
-}
-
-function getVideoId(video) {
-    const id = video.href.split("&")[0].slice(-11);
-    return id;
-}
-
-function refresh() {
-    const videos = getVideos();
-    for (let objVideo of videos) {
-        let strIdent = getVideoId(objVideo);
+    for (let objVideo of objVideos) {
+        let strIdent = objVideo.href.split('&')[0].slice(-11);
         let strTitle = '';
 
         mark(objVideo, strIdent);
 
-        if (!videoObserverMap.has(objVideo)) {
-            addVideoObserver(objVideo);
-        }
+        observe(objVideo);
 
         if (intWatchdate.hasOwnProperty(strIdent) === true) {
             continue;
@@ -64,17 +49,17 @@ function refresh() {
             if (objResponse !== null) {
                 intWatchdate[objResponse.strIdent] = objResponse.intTimestamp;
 
-                for (let objVideo of getVideos(objResponse.strIdent)) {
+                for (let objVideo of videos(objResponse.strIdent)) {
                     mark(objVideo, objResponse.strIdent);
                 }
             }
         });
     }
     
-    strLastchange = window.location.href + ':' + window.document.title + ':' + videos.length;
-}
+    strLastchange = window.location.href + ':' + window.document.title + ':' + objVideos.length;
+};
 
-function mark(objVideo, strIdent) {
+let mark = function(objVideo, strIdent) {
     if ((intWatchdate.hasOwnProperty(strIdent) === true) && (objVideo.classList.contains('youwatch-mark') === false)) {
         objVideo.classList.add('youwatch-mark');
 
@@ -84,24 +69,27 @@ function mark(objVideo, strIdent) {
 
     } else if ((intWatchdate.hasOwnProperty(strIdent) !== true) && (objVideo.classList.contains('youwatch-mark') !== false)) {
         objVideo.classList.remove('youwatch-mark');
-        objVideo.removeAttribute("watchdate");
+
+        if (objVideo.hasAttribute('watchdate') === true) {
+            objVideo.removeAttribute('watchdate');
+        }
 
     }
-}
+};
 
-function addVideoObserver(video) {
-    const observer = new MutationObserver(function (mutations, observer) {
-        for (const mutation of mutations) {
-            if (mutation.type === "attributes" && mutation.attributeName === "href") {
-                const id = getVideoId(video);
-                mark(video, id);
-            }
-        }
+let observe = function(objVideo) {
+    if (objObservers.has(objVideo) === true) {
+        return;
+    }
+
+    let objObserver = new MutationObserver(function() {
+        mark(objVideo, objVideo.href.split('&')[0].slice(-11));
     });
 
-    observer.observe(video, {attributes: true, attributeFilter: ["href"]});
-    videoObserverMap.set(video, observer);
-}
+    objObserver.observe(objVideo, {'attributes': true, 'attributeFilter': ['href']});
+
+    objObservers.set(objVideo, objObserver);
+};
 
 // ##########################################################
 
@@ -112,7 +100,7 @@ chrome.runtime.onMessage.addListener(function(objData, objSender, funcResponse) 
     } else if (objData.strMessage === 'youtubeMark') {
         intWatchdate[objData.strIdent] = objData.intTimestamp;
 
-        for (let objVideo of getVideos(objData.strIdent)) {
+        for (let objVideo of videos(objData.strIdent)) {
             mark(objVideo, objData.strIdent);
         }
 
@@ -135,7 +123,7 @@ window.setInterval(function() {
     if (document.hidden === true) {
         return;
 
-    } else if (strLastchange === window.location.href + ':' + window.document.title + ':' + getVideos().length) {
+    } else if (strLastchange === window.location.href + ':' + window.document.title + ':' + videos('').length) {
         return;
 
     }
