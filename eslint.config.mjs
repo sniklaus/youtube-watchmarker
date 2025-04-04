7c4153f203a0c91ed2e3f72ff414872db195bd6a
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2025-04-05 05:09:37 +0800

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  eslintConfigPrettier,
 ]);
